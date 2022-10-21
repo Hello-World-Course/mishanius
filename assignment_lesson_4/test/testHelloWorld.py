ff53222273744372308ae25602d85b5ec4744639
Michael Ptitsyn <mishapt@amazon.com>
2022-10-21 12:37:17 +0300

    added test dir

--- assignment_lesson_4/test/testHelloWorld.py
+++ assignment_lesson_4/test/testHelloWorld.py
@@ -10,5 +10,4 @@ class AssignmentOneTest(unittest.TestCase):
         with redirect_stdout(output_holder):
             import assignment_lesson_4.helloWorld
             print_out = output_holder.getvalue()
-            self.assertEqual(print_out, "Hello World Ilove Python!\n")
-            print("test done")
+            self.assertEqual(print_out, "Hello World I love Python!\n")
