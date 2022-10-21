83d3d61156634b4a1fa624e62b250bde6314cd73
Michael Ptitsyn <mishapt@amazon.com>
2022-10-21 12:25:50 +0300

    added test dir

--- assignment_lesson_4/test/testHelloWorld.py
+++ assignment_lesson_4/test/testHelloWorld.py
@@ -8,6 +8,6 @@ class AssignmentOneTest(unittest.TestCase):
     def test_execute_std_out(self):
         output_holder = io.StringIO()
         with redirect_stdout(output_holder):
-            import assignments.assignment_lesson_4.helloWorld
+            import assignment_lesson_4.helloWorld
             print_out = output_holder.getvalue()
             self.assertEqual(print_out, "Hello World I love Python!\n")
